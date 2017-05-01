8ba9b88c7258ac6df4ffab6cf0097900228f4ce7
Andrew Polaskey <apolaskey@gmail.com>
2017-04-30 22:26:50 -0400

    Moving the editor to the actual editor project

--- app/renderer/controllers/HelloWorldController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import RadiantController from "./RadiantController";
-
-export default class HelloWorldController extends RadiantController {
-
-}
--- app/renderer/modules/HelloWorldModule.jsx
+++
@@ -1,8 +0,0 @@
-'use strict';
-import RadiantModule from "./RadiantModule";
-
-export default class HelloWorldModule extends RadiantModule {
-    constructor(controller, view) {
-        super(controller, view);
-    }
-}
